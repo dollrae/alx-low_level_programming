1e5f144a2e06a3069ff19f9b3e278604fc8be14c
judy <ndunguwjudy@gmail.com>
2023-04-25 13:44:48 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - program that prints _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar(' ');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	putchar('\n');
+	return (0);
+}

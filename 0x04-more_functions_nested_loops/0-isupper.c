c69eb59d92a7f890e3b92087d54578bd08bfe66a
judy <ndunguwjudy@gmail.com>
2023-04-28 01:21:49 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,17 @@
  * _isupper - uppercase letters
  * @c: char to check
  *
- * Return: 0 or 1
+ * Return: Always 0
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }

aaed8db2d0890f2f5521e8ff8ff9d466438c99e3
judy <ndunguwjudy@gmail.com>
2023-04-28 03:02:29 -0700

    0. isupper update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks for uppercase character
  * @c: char to check
- * Return: 1 for uppercase or 0 otherwise
+ * Return: 1 if c is  uppercase,0 otherwise
  */
 int _isupper(int c);
 {

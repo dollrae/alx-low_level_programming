8abca747e855a25c04f1ad54265900d17127f19d
judy <ndunguwjudy@gmail.com>
2023-06-02 09:06:41 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,22 +9,23 @@
  * Return: The number of nodes in h.
  */
 
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t n = 0;
 
 
 	while (h)
 	{
-		(h => str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 
 		else
-			printf("[%d] %s\n",  h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 
-		nodes++;
+		n++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (n);
 }
